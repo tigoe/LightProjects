4a6a32a36eab0a0d57d80f0395c1c386c50e1707
tigoe <tom.igoe@gmail.com>
2019-06-03 12:47:38 -0400

    Update SqueezeLamp001.ino

--- SqueezeBulbLamp/SqueezeLamp001/SqueezeLamp001.ino
+++ SqueezeBulbLamp/SqueezeLamp001/SqueezeLamp001.ino
@@ -7,15 +7,19 @@
   controls two LED channels on pins 4 and 5
 
   Circuit:
-      * MPX2102 attached to an LM358 op amp attached to pin A0.
-      * potentiometer on pin A1
-      * 12V LEDs on pins 4 and 5
+        MPX2102 attached to an LM358 op amp attached to pin A0.
+        potentiometer on pin A1
+        12V LEDs on pins 4 and 5
     See circuit drawing for details
 
   created 19 Jan 2019
   by Tom Igoe
 */
 #include <SimpleKalmanFilter.h>
+#include <Wire.h>
+#include "Adafruit_TCS34725.h"
+
+Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_700MS, TCS34725_GAIN_1X);
 
 // first 2 numbers are the amount of variation,
 // third is how fast the measurement moves. recommended: 0.01:
@@ -24,7 +28,7 @@ SimpleKalmanFilter filter(2, 2, 0.01);
 int intensity = 0;      // LED intensity (0-255)
 int lastEstimate = 0;   // last sensor estimate from the Kalman filter
 int peakValue = 0;      // peak value
-int threshold = 605;    // threshold for peak reading
+int threshold = 1000;    // threshold for peak reading
 
 void setup() {
   // initialize serial communication:
@@ -34,18 +38,18 @@ void setup() {
 void loop() {
   // read the input:
   float sensorValue = analogRead(A0);
+  Serial.print(sensorValue);
+  Serial.print(",");
   // calculate the estimated value using the Kalman Filter:
   float estimate = filter.updateEstimate(sensorValue);
- 
-// look for a peak, above the threshold:
+  Serial.println(estimate);
+  // look for a peak, above the threshold:
   if (estimate > peakValue && estimate > threshold) {
     peakValue = estimate;
   }
   if (estimate <= threshold) {
     if (peakValue > threshold) {
       // you have a peak value:
-      Serial.print("peak: ");
-      Serial.println(peakValue);
       // at each peak, increase the intensity by 10%
       intensity += 25;
       // keep it at or below 255:
