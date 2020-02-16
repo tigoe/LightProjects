ec47f23de08b2f0a6398b2631243acf506e5e946
tigoe <tom.igoe@gmail.com>
2020-02-16 10:33:54 -0500

    Update RotaryDialDimmer0003.ino

--- TelephoneDimmer/RotaryDialDimmer0003/RotaryDialDimmer0003.ino
+++ TelephoneDimmer/RotaryDialDimmer0003/RotaryDialDimmer0003.ino
@@ -1,10 +1,16 @@
 /*
+  Uses a rotary phone to set the brightness of a Hue lamp
+
+  created 22 Jun 2019
+  modified 15 Feb 2020
+  by Tom Igoe
 */
 #include <SPI.h>
-#include <WiFi101.h>
+//#include <WiFi101.h>
+#include <WiFiNINA.h>
 #include <ArduinoHttpClient.h>
 #include <Arduino_JSON.h>
-//#include <ArduinoLowPower.h>
+#include <ArduinoLowPower.h>
 #include "arduino_secrets.h"
 
 // make a wifi instance and a HttpClient instance:
@@ -12,72 +18,114 @@ WiFiClient wifi;
 HttpClient httpClient = HttpClient(wifi, SECRET_HUE_IP);
 
 // inputs for the rotary phone:
-const int rotaryPin = 5;
-const int endRotaryPin = 7;
+const int rotaryPin = 2;
+const int endRotaryPin = 3;
+const int cradleHookPin = 9;
+
+bool hungUp = true;
 
 // status variables for the rotary phone:
 volatile bool dialing = false;
 volatile int dialCount = 0;
+const int debounceDelay = 12;
+
+// status variable for the HTTP request?
 volatile bool requesting = false;
+
+unsigned long timeToSleep = 0;
+unsigned long sleepDelay = 5 * 60 * 1000;
+int wakeups = 0;
+int prevWakeups = 0;
 
 void setup() {
   //Initialize serial and wait for port to open:
   Serial.begin(9600);
-  while (!Serial); // wait for serial port to connect.
-
-  // attempt to connect to Wifi network:
-  while ( WiFi.status() != WL_CONNECTED) {
-    Serial.print("Attempting to connect to WPA SSID: ");
-    Serial.println(SECRET_SSID);
-    // Connect to WPA/WPA2 network:
-    WiFi.begin(SECRET_SSID, SECRET_PASS);
-    delay(2000);
-  }
-
-  // you're connected now, so print out the data:
-  Serial.print("You're connected to the network IP = ");
-  IPAddress ip = WiFi.localIP();
-  Serial.println(ip);
-
+  delay(5000);
+  
   // configure the inputs for the rotary phone:
   pinMode (rotaryPin, INPUT_PULLUP);
   pinMode (endRotaryPin, INPUT_PULLUP);
+  pinMode (cradleHookPin, INPUT_PULLUP);
   pinMode(LED_BUILTIN, OUTPUT);
   attachInterrupt(digitalPinToInterrupt(rotaryPin), count, FALLING);
-  attachInterrupt(digitalPinToInterrupt(endRotaryPin), dial, RISING);
+  attachInterrupt(digitalPinToInterrupt(endRotaryPin), dial,  CHANGE);
+  LowPower.attachInterruptWakeup(digitalPinToInterrupt(cradleHookPin), wakePhone, RISING);
 }
 
 void loop() {
+
+  if (wakeups != prevWakeups) {
+    if (!Serial) Serial.begin(9600);
+    delay(1000);
+    Serial.println("I woke up!");
+    prevWakeups = wakeups;
+    // reset the sleep timer:
+    timeToSleep = 0;
+  }
+
   if (!dialing && dialCount > 0) {
     Serial.println(dialCount);
     if (!requesting) {
       int brightness = 0;
-      // 0 is off, 9 is brightest:
-      if (dialCount < 10) {
-        brightness = (dialCount + 1) * 25;
+      // 1 is off, 10 is brightest:
+      if (dialCount <= 10) {
+        brightness = (dialCount - 1) * 25;
       }
       JSONVar hueLight;
-      hueLight["bri"] = brightness;
+      // have to send "on" before "bri":
       if (brightness > 0) {
         hueLight["on"] = true;
       } else {
         hueLight["on"] = false;
       }
-      sendRequest(7, hueLight);   // turn light on
+      if ( hueLight["on"]) {
+        hueLight["bri"] = brightness;
+      }
+      if  (WiFi.status() == WL_CONNECTED) {
+        sendRequest(2, hueLight);   // turn light on
+      }
     }
     // reset digit counter:
     dialCount = 0;
   }
-  //  } else {
-  //    // put WiFi to sleep:
-  //    // put processor to sleep:
-  //    //  LowPower.sleep();
-  //  }
+  // check to see if the phone is hung up:
+  if (!dialing) {
+    hungUp = digitalRead(cradleHookPin);
+  }
+
+  // if you're connected to WiFi, turn the LED on:
+  digitalWrite(LED_BUILTIN, (WiFi.status() == WL_CONNECTED));
+
+
+  if (!hungUp) {
+    if (WiFi.status() != WL_CONNECTED
+        && WiFi.status() <= WL_NO_SHIELD) {
+      Serial.println("Wifi status:" + String(WiFi.status()));
+      connectToWiFi();
+    }
+  } else {
+    // if the phone is hung up and the sleep timer is not set:
+    if (timeToSleep == 0) {
+      Serial.println("setting sleep timer");
+      // set a timer until sleep:
+      timeToSleep = millis() + sleepDelay;
+    }
+    // if the time has passed, sleep:
+    if (millis() >= timeToSleep) {
+      // disconnect the WiFi:
+      if (WiFi.status() == WL_CONNECTED) {
+        WiFi.disconnect();
+        Serial.println("Disconnected from WiFi");
+      }
+      Serial.println("going to sleep now");
+      digitalWrite(LED_BUILTIN, LOW);
+      LowPower.sleep();
+    }
+  }
 }
 
 void sendRequest(int light, JSONVar lightState) {
   requesting = true;
-  digitalWrite(LED_BUILTIN, requesting);
   // make a String for the HTTP request path:
   String request = "/api/" + String(SECRET_HUE_USER);
   request += "/lights/";
@@ -107,17 +155,40 @@ void sendRequest(int light, JSONVar lightState) {
   Serial.println(response);
   Serial.println();
   requesting = false;
-  digitalWrite(LED_BUILTIN, requesting);
 }
 
+void connectToWiFi() {
+  // attempt to connect to Wifi network:
+  while ( WiFi.status() != WL_CONNECTED) {
+    Serial.print("Attempting to connect to WPA SSID: ");
+    Serial.println(SECRET_SSID);
+    // Connect to WPA/WPA2 network:
+    WiFi.begin(SECRET_SSID, SECRET_PASS);
+    delay(2000);
+  }
+
+  // you're connected now, so print out the data:
+  Serial.print("You're connected to the network IP = ");
+  IPAddress ip = WiFi.localIP();
+  Serial.println(ip);
+}
+
+
+//  Interrupt handlers:
+
+void wakePhone() {
+  wakeups++;
+}
 void dial() {
-  dialing = false;
+  delay(debounceDelay);
+  dialing = !digitalRead(endRotaryPin);
+  if (dialing) {
+    dialCount = 0;
+  }
 }
 
 void count() {
-  dialing = true;
-  // slight delay to debounce internal contacts:
-  delayMicroseconds(11);
+  delay(debounceDelay);
   if (digitalRead(rotaryPin) == LOW) {
     dialCount++;
   }
