f80268ea09744641b30e86cc82b2b3cf6a2ed608
tigoe <tom.igoe@gmail.com>
2020-02-19 07:44:38 -0500

    Update WarmCoolLEDStrip.ino

--- WarmCoolLEDStrip/WarmCoolLEDStrip.ino
+++ WarmCoolLEDStrip/WarmCoolLEDStrip.ino
@@ -1,6 +1,6 @@
 /*
   Warm-Cool lamp using 12V LED strips
-  
+    
   Reads a potentiometer on pin A1, uses it to set a ratio of warm to cool
   controls two LED channels on pins 4 and 5
 
@@ -12,8 +12,6 @@
   modified 16 Feb 2020
   by Tom Igoe
 */
-int intensity = 0;      // LED intensity (0-255)
-
 void setup() {
   // initialize serial communication:
   Serial.begin(9600);
@@ -22,9 +20,10 @@ void setup() {
 void loop() {
   // read potentiometer, use it to set ratio of warm to cool:
   int potReading = analogRead(A0) / 4;
-  int warm = intensity * (255 - potReading) / 255;
-  int cool = intensity * (potReading / 255);
+  int warm =  255 - potReading;
+  int cool =  potReading;
+
   // PWM the LED channels:
-  analogWrite(4, warm);
   analogWrite(5, cool);
+  analogWrite(6, warm);
 }
