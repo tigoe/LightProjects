80d042fc1c52c4eb29fa59909b32dbf1a58d558e
tigoe <tom.igoe@gmail.com>
2020-04-08 13:28:11 -0400

    gitignore

--- LightSensorProjects/LightSensorChart/credentials.js
+++ LightSensorProjects/LightSensorChart/credentials.js
@@ -1,8 +1,8 @@
 const credentials = {
-   serverUrl : "",
+   serverUrl : "tigoe.io",
    route : "/data",
-	macAddress : "",
-	sessionKey : ""
+	macAddress : "3c:71:bf:87:ce:4c",
+	sessionKey : "1D132679-A0C9-4410-8C3C-46B87BB553E0"
 };
 
 document.getElementById('url').value = credentials.serverUrl;
