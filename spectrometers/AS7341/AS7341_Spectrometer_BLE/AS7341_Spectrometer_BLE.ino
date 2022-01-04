d4d839a58e758ccfe683adaf8b99c1340bf0b63a
Tom Igoe <tom.igoe@gmail.com>
2022-01-04 18:05:55 -0500

    added BLE spectrometer

---
+++ spectrometers/AS7341/AS7341_Spectrometer_BLE/AS7341_Spectrometer_BLE.ino
@@ -0,0 +1,164 @@
+/*
+  AS7341 sensor readings.  Reads and sends the results via BLE
+  as a CSV string. This version uses a non-blocking
+  approach, using the startReading() and checkReadingProgress()
+  functions. It's about 2 seconds between readings.
+
+  Library:
+  http://librarymanager/All#Adafruit_AS7341
+  http://librarymanager/All#ArduinoBLE
+
+  created 18 Jun 2021
+  modified 4 Jan 2022
+  by Tom Igoe
+*/
+
+#include <Adafruit_AS7341.h>
+#include <ArduinoBLE.h>
+
+// the uuid for the service. To make the uuids for the characteristics,
+// replace the 0000 with higher values (0001, etc):
+char serviceUuid[] = "9af01fc3-0000-44b8-8acc-f3ed7a225431";
+char characteristicUuid[] = "9af01fc3-0001-44b8-8acc-f3ed7a225431";
+
+// fill in your device name here. It will be combined with
+// the BLE MAC address in the setup function to set the peripheral BLE name:
+String bleName = "spectro-";
+
+// instance of the sensor library:
+Adafruit_AS7341 as7341;
+
+// array to hold the raw readings:
+uint16_t readings[12];
+// readingString will be 50 bytes in length:
+const int readingLength = 50;
+// string for readings:
+String readingString;
+
+
+// BLE service and characteristic:
+BLEService spectroService(serviceUuid);
+// create sensor characteristic and allow remote device to get notifications:
+BLECharacteristic spectroCharacteristic(characteristicUuid, BLERead | BLENotify, readingLength);
+
+
+void setup() {
+  // init serial, wait 3 secs for serial monitor to open:
+  Serial.begin(9600);
+  // if the serial port's not open, wait 3 seconds:
+  if (!Serial) delay(3000);
+
+  // readingString will need 50 bytes for all values:
+  readingString.reserve(readingLength);
+
+  if (!as7341.begin()) {
+    Serial.println("Sensor not found, check wiring");
+    //    while (true);
+  } else {
+    // set integration time:
+    as7341.setATIME(35);
+    as7341.setASTEP(10000);
+    as7341.setGain(AS7341_GAIN_256X);
+    // start a new reading cycle:
+    as7341.startReading();
+  }
+
+  // begin initialization
+  if (!BLE.begin()) {
+    Serial.println("starting BLE failed");
+    while (true);
+  }
+
+  // set the local name peripheral advertises:
+  // first, add the BLE address:
+  bleName += BLE.address();
+  // then convert the String to an constant char array
+  // in order to set name:
+  BLE.setLocalName(bleName.c_str());
+  // finally, print it:
+  Serial.println(bleName);
+  // set the UUID for the service this peripheral advertises:
+  BLE.setAdvertisedService(spectroService);
+
+  // add the characteristic to the service
+  spectroService.addCharacteristic(spectroCharacteristic);
+
+  // add the service
+  BLE.addService(spectroService);
+  spectroCharacteristic.writeValue(readingString.c_str());
+
+  // start advertising
+  BLE.advertise();
+  Serial.println("Bluetooth device active, waiting for connections...");
+}
+
+
+void loop() {
+  // listen for BLE peripherals to connect:
+  BLEDevice central = BLE.central();
+  // if one connects:
+  if (central) {
+    // print the central's MAC address:
+    Serial.print("Connected to central: ");
+    Serial.println(central.address());
+
+    // while it's connected, get sensor readings:
+    while (central.connected()) {
+      // read the sensor, print if there are good values:
+      // if (readSensor()) {
+
+      if (millis() % 2000 < 3) {
+        readingString = "";
+        // print the adjusted readings:
+        for (int r = 0; r < 10; r++) {
+          readingString += String(random(100) / 100.0);
+          // print a comma for all but the last reading:
+          if (r < 9) readingString += ",";
+        }
+
+        spectroCharacteristic.writeValue(readingString.c_str());
+        Serial.println(readingString);
+      }
+    }
+    Serial.println("Disconnected");
+  }
+}
+
+bool readSensor() {
+  // clear the reading string:
+  readingString = "";
+  // check to see if readings are done
+  //  checkReadingProgress() is non-blocking:
+
+  float result = as7341.checkReadingProgress();
+  // if there is a reading, do corrections and parse it
+  // into readingString:
+  if (result) {
+    // correction factors:
+    // TODO: This is crude, and could be better with matrix math.
+    // from https://ams.com/documents/20143/36005/AS7341_AN000633_1-00.pdf/fc552673-9800-8d60-372d-fc67cf075740
+    // fig 10
+
+    // Added in 0 for 4 and 5 since those channels are not used below.
+    float corrections[] = {3.20, 3.00, 2.07, 1.30, 0, 0, 1.07, 0.93, 0.78, 0.71};
+
+    // get the readings:
+    as7341.getAllChannels(readings);
+    // loop over the readings and print them out:
+    for (int r = 0; r < 12; r++) {
+      // skip 4 and 5 as they are repeats:
+      if (r == 4 || r == 5) continue;
+      float thisReading = as7341.toBasicCounts(readings[r]);
+      // if this is one of the channels with a correction, apply it:
+      if (r < 10) {
+        thisReading = thisReading * corrections[r];
+      }
+      readingString += String(thisReading);
+      if (r < 11) readingString += ",";
+    }
+
+    // start a new reading cycle:
+    as7341.startReading();
+  }
+  return result;
+}
---
+++ spectrometers/AS7341/spectrograph_chartjs_ble/sketch.js
@@ -0,0 +1,236 @@
+/*
+  Spectrograph
+  Written for serial input from AS7341, but could be adapted
+  for other spectrometers, and for other transport mechanisms.
+
+  Assumes a set of comma-separated values.
+
+  uses p5.js, p5BLE.js and chart.js. 
+  For more on chart.js, see https://www.chartjs.org/
+  For more on the p5BLE.js: https://itpnyu.github.io/p5ble-website/
+  
+  created 4 Jan 2021
+  by Tom Igoe
+*/
+
+// HTML divs for messages
+let textDiv;
+let timestampDiv;
+
+// fill in wavelengths of your spectrometer here:
+let wavelengths = [415, 445, 480, 515, 555, 590, 630, 680, 910, 'clear'];
+let numBands = 10;
+
+// array to put the readings from the sensors into:
+let readings = new Array();
+
+// instance of the chart:
+let chart;
+
+// advertised service UUID of the  to search for:
+const serviceUuid = "9af01fc3-0000-44b8-8acc-f3ed7a225431";
+// characteristic that you plan to read:
+let myCharacteristic;
+// instance of p5.ble:
+let myBLE;
+// DOM elements to interact with:
+let connectButton;
+
+// data object for the chart:
+const data = {
+  labels: wavelengths,
+  datasets: [{
+    label: 'AS7341 Spectrometer',
+    data: new Array(),
+    backgroundColor: new Array()
+  }]
+};
+
+const config = {
+  type: 'bar',
+  data: data,
+  options: {
+    plugins: {
+      // this gets rid of the box by the data label:
+      legend: {
+        labels: {
+          boxWidth: 0
+        }
+      }
+    },
+    // change the animation from one reading to the next:
+    animation: {
+      duration: 200 // in ms
+    }
+  }
+};
+
+function setup() {
+  // set up the canvas:
+  createCanvas(800, 600);
+
+  // Create a p5ble instance:
+  myBLE = new p5ble();
+
+  // Create a 'Connect' button
+  connectButton = createButton('Connect')
+  connectButton.mousePressed(connectToBle);
+  connectButton.position(10, 30);
+
+  // create a div for local messages:
+  textDiv = createDiv('Waiting for BLE connection');
+  textDiv.position(20, 10);
+
+  // instantiate the chart:
+  chart = new Chart(
+    this,   // context: this sketch
+    config  // config from the global variables
+  );
+  // push each wavelength onto the chart's
+  // background color and borderColor:
+  for (w of wavelengths) {
+    // if the band name  in the visible spectrum, calculate its color:
+    if (w > 400 && w < 700) {
+      chart.data.datasets[0].backgroundColor.push(wavelengthToColor(w));
+    }
+    else {
+      if (w > 900) { // for near IR, use deep red:
+        chart.data.datasets[0].backgroundColor.push('#770000');
+      } else { // for 'clear', use black as its color:
+        chart.data.datasets[0].backgroundColor.push('#000000');
+      }
+    }
+  }
+  // make a text div for the timestamp of each reading:
+  timestampDiv = createDiv('last reading at: ');
+  timestampDiv.position(windowWidth - 300, 10);
+}
+
+function draw() {
+  // update the chart:
+  chart.update();
+}
+
+
+function connectToBle() {
+  // Connect to a device by passing the service UUID
+  myBLE.connect(serviceUuid, gotCharacteristics);
+}
+
+// A function that will be called once got characteristics
+function gotCharacteristics(error, characteristics) {
+  // if there's an error, 
+  // notify the user and quit the function:
+  if (error) {
+    console.log(error);
+    textDiv.innerHTML = 'error: ' + error;
+    return;
+  }
+
+  if (myBLE.isConnected()) {
+    connectButton.html('Disconnect');
+    connectButton.mousePressed(disconnectBle);
+  } else {
+    connectButton.html('Connect');
+    connectButton.mousePressed(connectToBle);
+  }
+
+  myBLE.onDisconnected(connectionChanged);
+  console.log('characteristics: ', characteristics);
+  myCharacteristic = characteristics[0];
+  // start notifications:
+  myBLE.startNotifications(myCharacteristic, handleNotifications, 'string');
+}
+
+
+// A function that will be called once got characteristics
+function handleNotifications(data) {
+  // Split the message into an array:
+  let readings = float(split(data, ','));
+  // if you have all the readings, put them in the chart:
+  if (readings.length >= numBands) {
+    chart.data.datasets[0].data = readings;
+    // update the timestamp:
+    timestampDiv.html('last reading at: ' + new Date().toLocaleString());
+  }
+  textDiv.html(data);
+}
+
+function disconnectBle() {
+  // Disonnect  the device
+  myBLE.disconnect();
+  // Check if myBLE is connected
+  isConnected = myBLE.isConnected();
+}
+
+function connectionChanged() {
+  if (myBLE.isConnected()) {
+    connectButton.html('Disconnect');
+    connectButton.mousePressed(disconnectBle);
+  } else {
+    connectButton.html('Connect');
+    connectButton.mousePressed(connectToBle);
+  }
+
+  console.log('Device state:');
+  isConnected = myBLE.isConnected();
+  console.log(isConnected);
+}
+
+// takes wavelength in nm and returns an rgba value
+// adapted from https://scienceprimer.com/javascript-code-convert-light-wavelength-color
+function wavelengthToColor(wavelength) {
+  var r, g, b, alpha, colorSpace,
+    wl = wavelength,
+    gamma = 1;
+  // UV to indigo:
+  if (wl >= 380 && wl < 440) {
+    R = -1 * (wl - 440) / (440 - 380);
+    G = 0;
+    B = 1;
+    // indigo to blue:
+  } else if (wl >= 440 && wl < 490) {
+    R = 0;
+    G = (wl - 440) / (490 - 440);
+    B = 1;
+    // blue to green:
+  } else if (wl >= 490 && wl < 510) {
+    R = 0;
+    G = 1;
+    B = -1 * (wl - 510) / (510 - 490);
+    // green to yellow:
+  } else if (wl >= 510 && wl < 580) {
+    R = (wl - 510) / (580 - 510);
+    G = 1;
+    B = 0;
+    // yellow to orange:
+  } else if (wl >= 580 && wl < 645) {
+    R = 1;
+    G = -1 * (wl - 645) / (645 - 580);
+    B = 0.0;
+    // orange to red:
+  } else if (wl >= 645 && wl <= 780) {
+    R = 1;
+    G = 0;
+    B = 0;
+    // IR:
+  } else {
+    R = 0;
+    G = 0;
+    B = 0;
+  }
+
+  // intensity is lower at the edges of the visible spectrum.
+  if (wl > 780 || wl < 380) {
+    alpha = 0;
+  } else if (wl > 700) {
+    alpha = (780 - wl) / (780 - 700);
+  } else if (wl < 420) {
+    alpha = (wl - 380) / (420 - 380);
+  } else {
+    alpha = 1;
+  }
+  // combine it all:
+  colorSpace = "rgba(" + (R * 100) + "%," + (G * 100) + "%," + (B * 100) + "%, " + alpha + ")";
+  return colorSpace;
+}
