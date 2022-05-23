64efde9603d5a905a6b8667639a55a0c365c3b83
Tom Igoe <tom.igoe@gmail.com>
2022-05-23 17:14:32 -0400

    Update SineFadeWithInput.ino

--- FadeCurves/SineFadeWithInput/SineFadeWithInput.ino
+++ FadeCurves/SineFadeWithInput/SineFadeWithInput.ino
@@ -6,13 +6,20 @@
   - LED attached to pin 5
   - 10Kilohm potentiometer attached to A0
 
+
+  to change from 10-bit resolution to 8, change the resolution variable
+  and comment out the analogWriteResolution() command
+ 
   created 30 Jan 2019
-  modified 9 June 2019
+  modified 23 May 2022
   by Tom Igoe and lighting & interactivity 2019 class
 */
 int currentLevel = 255;
 int change = 1;
-byte levelTable[256]; // pre-calculated PWM levels
+const int resolution = 10;
+const int steps = pow(2, resolution);
+
+byte levelTable[steps]; // pre-calculated PWM levels
 
 void setup() {
   Serial.begin(9600);
@@ -24,7 +31,7 @@ void loop() {
   // read potentiometer:
   int sensorReading = analogRead(A0);
   // map to 0-255 range:
-  int currentLevel = map(sensorReading, 0, 1023, 0, 255);
+  int currentLevel = map(sensorReading, 0, 1023, 0, steps);
   //PWM output the result:
   analogWrite(5, levelTable[currentLevel]);
   Serial.println(levelTable[currentLevel]);
@@ -33,7 +40,7 @@ void loop() {
 
 void fillLevelTable() {
   // set the range of values:
-  float maxValue = 255;
+  float maxValue = steps;
 
   // iterate over the array and calculate the right value for it:
   for (int l = 0; l <= maxValue; l++) {
@@ -53,7 +60,7 @@ void fillLevelTable() {
       result = result * 127.5;
     */
     //here it all is in one line:
-    float lightLevel = (sin((angle * PI / 180) + PI / 2) + 1) * 127.5;
+    float lightLevel = (sin((angle * PI / 180) + PI / 2) + 1) * (steps/2);
     levelTable[l] = lightLevel;
   }
 }
