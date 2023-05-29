d71846c4baaa5bf11667f57b389e5c1ac0a48243
Tom Igoe <tom.igoe@gmail.com>
2023-05-29 17:10:40 -0400

    add qrcode

---
+++ img/qrcode.js
@@ -0,0 +1,15 @@
+// add a QR code of the URL when the page loads
+function getQrCode() {
+  // make the QR code:
+  let qr = qrcode(0, 'L');
+  qr.addData(document.URL);
+  qr.make();
+  // create an image from it:
+  let qrImg = qr.createImgTag(2, 8, "qr code of " + document.URL);
+ // get the image and label:
+  let label = document.getElementById('qrcode');
+  label.innerHTML = qrImg + '<br>' + document.URL;
+}
+
+// on page load, call the QR code function:
+document.addEventListener('DOMContentLoaded', getQrCode);
